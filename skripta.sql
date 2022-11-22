0e3ade821c4194de45099da5fb4747a7488ec4d1
Marko Pavlović <markopavlovic316@gmail.com>
2022-11-22 17:56:58 +0100

    Unošenje podataka u tablice

--- skripta.sql
+++ skripta.sql
@@ -4,7 +4,7 @@
 
 drop database if exists edunovapp26;
 # BRISE STARU BAZU PODATAKA AKO VEC POSTOJI DA BI MOGLI NAPRAVITI NOVU VERZIJU
-create database edunovapp26;
+create database edunovapp26 character set utf8;
 # KREIRA NOVU BAZU PODATAKA "EDUNOVAPP26"
 use edunovapp26;
 # KORISTIMO NOVU BAZU PODATAKA DA BI U NJU POHRANILI NOVE ENTITETE I VRSTE PODATAKA
@@ -15,7 +15,7 @@ create table smjer(
     cijena decimal(18,2),
     upisnina decimal(18,2),
     certificiran boolean not null,
-    trajanje int not null,
+    trajanje int not null
 );
 # KREIRA TABLICU UNUTAR BAZE 
 # "INT NOT NULL PRIMARY KEY AUTO_INCREMENT" OZNACAVA DA CE BAZA ZA SVAKI NOVI UNOS GENERIRATI POSEBNU SIFRU
@@ -72,4 +72,66 @@ alter table predavac add foreign key (osoba) references osoba(sifra);
 
 # ALTER TABLE [IME ENTITETA OD KOJEG POLAZI VANJSKI KLJUČ] ADD FOREIGN KEY [IME ENTITETA NA KOJEM ZAVRŠAVA VANJSKI KLJUČ]
 # REFERENCES [IME ATRIBUTA KOJE DIJELI ENTITET] (IME ATRIBUTA KOJI SE PRENOSI U PRETHODNI ATRIBUT)
+
+# 1 -  vrijednost sifre u tablici smjer
+insert into smjer (sifra,naziv,certificiran,trajanje,cijena,upisnina)
+values(null,'PHP Programiranje',false,130,6999.99,500);
+
+# 2 -  vrijednost sifre u tablici smjer
+insert into smjer (sifra,naziv,certificiran,trajanje,cijena,upisnina)
+values(null,'Java Programiranje',true,150,9999.99,500);
+
+insert into grupa (sifra,naziv,maksimalnopolaznika,datumpocetka,smjer)
+values
+(null,'PP26',20,'2022-11-07 19:00:00',1),
+(null,'JP27',20,'2022-11-07 17:00:00',2);
+
+# UNOS VRIJEDNOSTI U TABLICE
+# insert into [IME TABLICE] (ATRIBUTI U KOJE CEMO UNOSITI)
+# values (VRIJEDNOSTI ZA ATRIBUTE U KOJE SMO REKLI DA UNOSIMO ODVOJENI ZAREZOM)
+
+# MOGUC JE UNOS VISE PODATAKA OD JEDNOM PREKO PRIMJERA IZNAD (SAMO SE NAKON ZAVRSETKA ZAGRADE STAVI ZAREZ I PONOVE PODATCI)
+# VANJSKI KLJUCEVI SE MORAJU DEFINIRATI SIFRAMA OD PRIPADAJUCIH ENTITETA 
+
+
+# 1 - 20
+insert into osoba (sifra,ime,prezime,email)
+values
+(null,'Adam','Vicković','vickovic2203@gmail.com'),
+(null,'Boris','Bukovec','botaosijek@gmail.com'),
+(null,'Darija','Dumančić','darija.zdarilek@gmail.com'),
+(null,'Darijan','Petrač','darijan.petrac@gmail.com'),
+(null,'Domagoj','Čuli','domo.culi@gmail.com'),
+(null,'Filip','Janješić','filip.janjesic@gmail.com'),
+(null,'Iris','Matokić','irismatokic@gmail.com'),
+(null,'Ivan','Sambol','ivan.sambol@skole.hr'),
+(null,'Kristijan','Lovrić','klovric991@gmail.com'),
+(null,'Lobel','Špehar','lobel.spehar.os@gmail.com'),
+(null,'Luka','Agić','agic.luke@gmail.com'),
+(null,'Marko','Mikulić','marko_mikulic08@hotmail.com'),
+(null,'Marko','Pavlović','markopavlovic316@gmail.com'),
+(null,'Nemanja','Đurić','nemanja.duric92@gmail.com'),
+(null,'Stjepan','Abramović','stjepanabramovic1@gmail.com'),
+(null,'Sven','Ostojić','sven.ostojic@outlook.com'),
+(null,'Tonko','Vorgić','tonko85@gmail.com'),
+(null,'Jakov','Beg','jakovbeg@gmail.com'),
+(null,'Tomislav','Ružičić','truzicic@gmail.com'),
+(null,'Tomislav','Jakopec','tjakopec@gmail.com');
+
+
+# 1 - 19
+insert into polaznik (sifra,osoba)
+values
+(null,1), (null,2), (null,3), (null,4), (null,5), (null,6), (null,7), (null,8), (null,9), (null,10), (null,11), (null,12),
+(null,13), (null,14), (null,15), (null,16), (null,17), (null,18), (null,19); 
+
+# 1
+insert into predavac (sifra,osoba)
+values (null,20);
+
+# 1 - 19
+insert into clan(grupa,polaznik)
+values
+(1,1), (1,2), (1,3), (1,4), (1,5), (1,6), (1,7), (1,8), (1,9), (1,10), (1,11), (1,12), (1,13), (1,14), (1,15), (1,16), (1,17), (1,18), (1,19);
+
 
