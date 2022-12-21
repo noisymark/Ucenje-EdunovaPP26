181b6ddccfa11a9b7210930494ac1cd30d405dc6
Marko Pavlović <markopavlovic316@gmail.com>
2022-12-21 16:37:07 +0100

    Adding icon to the website

--- www/ucenjephp.hr/head.php
+++ www/ucenjephp.hr/head.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Foundation for Sites</title>
+<title>Učenje PHP</title>
 <link rel="stylesheet" href="css/foundation.css">
 <link rel="stylesheet" href="css/app.css">
+<link rel="shortcut icon" href="icon.png" type="image/x-icon">
