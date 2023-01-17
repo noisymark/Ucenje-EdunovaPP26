7278df832a9477910a314038fc8cd7950e96e43b
Marko Pavlović <markopavlovic316@gmail.com>
2023-01-17 20:02:39 +0100

    FINALLY COMPLETED THE CIRCULANT MATRIX TABLE
    Starting from down right and going clockwise

--- www/ucenjephp.hr/ciklicnatablica.php
+++ www/ucenjephp.hr/ciklicnatablica.php
@@ -1,71 +1,60 @@
-<?php
+<?php 
+            $rss=isset($_GET['broj1']) ? $_GET['broj1'] : 0;
+            $css=isset($_GET['broj2']) ? $_GET['broj2'] : 0;
 
-// Defining all the neccessary variables that will be used
-$broj1 = isset($_GET['broj1']) ? $_GET['broj1'] : 0;
-$broj2 = isset($_GET['broj2']) ? $_GET['broj2'] : 0;
-// Converting strings to int type of value
-$br1 = (int)$broj1;
-$br2 = (int)$broj2;
-// Caluclating the result of multiplication of these 2 numbers
-$br3 = (int)($br1*$br2);
-//Implementing a value to be filled in matrix
-$val=1;
-$a=[];
+            $rows=(int)$rss;
+            $columns=(int)$css;
 
-//br1 = broj stupaca 
-//br2 = broj redova
-//br3 = umnozak redova i stupaca
-
-$pir = 0;
-$zir = $br1;
-$pis = 0;
-$zis = $br2;
-
-//pir = pocetni index reda
-//zir = zavrsni index reda
-//pis = pocetni index stupca
-//zis = zavrsni index stupca
-
-for($pir=0;$pir < $zir && $pis < $zis;){
-        for($i = $zir; $i < $zis; ++$i){
-                $a[$pir][$i] = $val++;
-        }
-        $pir++;
-
-        for ($i = $pir; $i < $zir; ++$i){
-            $a[$i][$zis - 1] = $val++;
-        }
-        $zis--;
-
-        if ($pir < $zir)
-        {
-            for ($i = $zis - 1; $i >= $pis; --$i){
-                $a[$zir - 1][$i] = $val++;
+            if($rows===0 || $columns===0){
+                echo 'UNESITE OBA BROJA KAO VALJANU VRIJEDNOST CIJELOG BROJA!';
             }
-            $m--;
-        }
 
-        if ($pis < $zis)
+            $maxRow=$rows-1;
+            $maxCol=$columns-1;
+            $ispis=1;
+            $matrica=[];
+            $minCol=0;
+            $minRow=0;
+            while ($ispis <= $rows*$columns) 
+            {
+                for($j=$maxCol; $j >= $minCol; $j--)
+                {
+                    $matrica[$maxRow][$j] = $ispis++;
+                }
+                    $maxRow--;
+                    if ($ispis > $rows*$columns) break;
+
+                for ($i=$maxRow; $i >= $minRow ;$i--) 
+                { 
+                    $matrica[$i][$minCol] = $ispis++;
+                }
+                    $minCol++;
+                    if ($ispis > $rows*$columns) break;
+
+                for ($j=$minCol; $j <= $maxCol; $j++) 
+                { 
+                    $matrica[$minRow][$j] = $ispis++;
+                }
+                    $minRow++;
+                    if ($ispis > $rows*$columns) break;
+                for ($i=$minRow; $i <= $maxRow; $i++) 
+                { 
+                    $matrica[$i][$maxCol] = $ispis++;
+                }
+                    $maxCol--;    
+            
+            }
+
+echo '<table style="text-align:center;">';
+        for($i=0;$i<$rows;)
         {
-            for ($i = $zir - 1; $i >= $pir; --$i)
-                $a[$i][$pis] = $val++;
-            $pis++;
-        }
-
-}
-
-
-//echo '<pre>';
-//var_dump($red);
-//var_dump($stupac);
-//echo '</pre>';
-
-for ($i = 0; $i < $zir; $i++)
-{
-    for ($j = 0; $j < $zis; $j++)
-    {
-        echo ($a[$i][$j]);
-        echo (" ");
-    }
-    echo ("\n");
-}
+          echo '<tr>';
+            for ($j=0; $j < $columns;) 
+            { 
+              echo '<td>'. $matrica[$i][$j] .'</td>';
+              $j++;
+            }
+            echo '</tr>';
+            $i++;
+        } 
+echo '</table>';
