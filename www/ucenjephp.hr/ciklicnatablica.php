48c3fde9eefec95058c436a3ef5a111245460ab8
Marko Pavlović <markopavlovic316@gmail.com>
2023-01-17 17:20:17 +0100

    Another try to solve circular matrix and an example how it should look

--- www/ucenjephp.hr/ciklicnatablica.php
+++ www/ucenjephp.hr/ciklicnatablica.php
@@ -4,29 +4,68 @@
 $broj1 = isset($_GET['broj1']) ? $_GET['broj1'] : 0;
 $broj2 = isset($_GET['broj2']) ? $_GET['broj2'] : 0;
 // Converting strings to int type of value
-$br1 = (int) $broj1;
-$br2 = (int) $broj2;
+$br1 = (int)$broj1;
+$br2 = (int)$broj2;
 // Caluclating the result of multiplication of these 2 numbers
-$br3 = (int) $br1 * $br2;
-//Implementing an empty array
-$aa=[];
-// For every instance of the loop adding the current loop number to the end of the array
-// So that in the end, the array will start with the highest number and end with 0
-for($i=$br1*$br2;$i>0;$i--){
-    array_push($aa,$i);
-}
-// Displaying the results in RAW mode
-echo '<pre>';
-var_dump($aa);
-echo '</pre>';
+$br3 = (int)($br1*$br2);
+//Implementing a value to be filled in matrix
+$val=1;
+$a=[];
 
-echo '<table style="text-align:center;">';
-for($i=$br2;$i>0;$i--){
-    echo '<tr>';
-        for($j=$br1;$j>0;$j--){
-            echo '<td>' . $br3 . '</td>';
-            $br3-=1;
+//br1 = broj stupaca 
+//br2 = broj redova
+//br3 = umnozak redova i stupaca
+
+$pir = 0;
+$zir = $br1;
+$pis = 0;
+$zis = $br2;
+
+//pir = pocetni index reda
+//zir = zavrsni index reda
+//pis = pocetni index stupca
+//zis = zavrsni index stupca
+
+for($pir=0;$pir < $zir && $pis < $zis;){
+        for($i = $zir; $i < $zis; ++$i){
+                $a[$pir][$i] = $val++;
         }
-    echo '</tr>';
+        $pir++;
+
+        for ($i = $pir; $i < $zir; ++$i){
+            $a[$i][$zis - 1] = $val++;
+        }
+        $zis--;
+
+        if ($pir < $zir)
+        {
+            for ($i = $zis - 1; $i >= $pis; --$i){
+                $a[$zir - 1][$i] = $val++;
+            }
+            $m--;
+        }
+
+        if ($pis < $zis)
+        {
+            for ($i = $zir - 1; $i >= $pir; --$i)
+                $a[$i][$pis] = $val++;
+            $pis++;
+        }
+
 }
-echo '</table>';
+
+
+//echo '<pre>';
+//var_dump($red);
+//var_dump($stupac);
+//echo '</pre>';
+
+for ($i = 0; $i < $zir; $i++)
+{
+    for ($j = 0; $j < $zis; $j++)
+    {
+        echo ($a[$i][$j]);
+        echo (" ");
+    }
+    echo ("\n");
+}
